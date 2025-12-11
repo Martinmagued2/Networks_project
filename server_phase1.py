965f3a00aa4a5da026db7412d7e066dd64f75686
George Hany <georgehany562002@gmail.com>
2025-12-11 19:14:36 +0200

    made the header format and Identified message types

--- server_phase1.py
+++ server_phase1.py
@@ -14,3 +14,38 @@ def create_socket():
 def receive_packet(sock):
     return sock.recvfrom(BUFFER_SIZE)
 
+HEADER_FORMAT = '<B H H L B'
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+
+# Message type constants
+MSG_INIT = 0
+MSG_DATA = 1
+MSG_HEARTBEAT = 2
+
+def parse_header(data):
+    if len(data) < HEADER_SIZE:
+        return None, "Malformed"
+
+    version_msgtype, device_id, seq, ts, flags = struct.unpack(
+        HEADER_FORMAT, data[:HEADER_SIZE]
+    )
+
+    version = version_msgtype >> 4
+    msg_type = version_msgtype & 0x0F
+
+    msg_name = {
+        MSG_INIT: "INIT",
+        MSG_DATA: "DATA",
+        MSG_HEARTBEAT: "HEARTBEAT"
+    }.get(msg_type, "UNKNOWN")
+
+    return {
+        "version": version,
+        "msg_type": msg_type,
+        "msg_name": msg_name,
+        "device_id": device_id,
+        "seq": seq,
+        "timestamp": ts,
+        "flags": flags
+    }, None
+
